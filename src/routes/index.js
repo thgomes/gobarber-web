98318421338a54d5cb86bda5b5ecf7cf4496e12e
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-05-17 15:56:22 -0300

    estilização tela de login e cadastro

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="register" component={SignUp} />
+      <Route path="/register" component={SignUp} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
