1cfa5a64069bd2e0a64b53978bf2a1bc26aa87be
Thiago Gomes <thiagosiqgomes@gmail.com>
2020-06-08 14:29:27 -0300

    Dashboard

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -31,6 +32,8 @@ module.exports = {
       'warn',
       { extensions: ['.js', '.jsx'] }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
 };
